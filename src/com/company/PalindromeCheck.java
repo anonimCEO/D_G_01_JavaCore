055f5dea2c14518361ac70fddf8b69f980751a71
denis <anonim.ceo@gmail.com>
2020-03-20 14:11:54 +0200

    Test is palindrome added

--- src/com/company/PalindromeCheck.java
+++ src/com/company/PalindromeCheck.java
@@ -2,17 +2,16 @@ package com.company;
 
 public class PalindromeCheck {
 
-    public static String isPalindrome(String palindromeNumber){
+    public static boolean isPalindrome(String palindromeNumber){
 
-        String s = palindromeNumber.replaceAll("\\s","");
+        String s = palindromeNumber.toLowerCase().replaceAll("\\s","");
 
-        if (s.equals(StringReverse.stReverse(s))){
-            System.out.println(palindromeNumber + " - is a palindrome");
-        }else {
-            System.out.println(palindromeNumber + " - is not a palindrome");
-        }
-        return palindromeNumber;
+        // System.out.println(palindromeNumber + " - is a palindrome");
+        // System.out.println(palindromeNumber + " - is not a palindrome");
+        if (!s.matches("^([a-z0-9]+)?$")) {
+            //System.out.println(palindromeNumber + " - is not a palindrome");
+            return false;
+        }else return s.equals(StringReverse.stReverse(s));
+
     }
-
-
 }
--- src/test/PalindromeTest.java
+++ src/test/PalindromeTest.java
@@ -3,27 +3,39 @@ package test;
 import com.company.PalindromeCheck;
 import com.company.StringReverse;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
-public class PalindromeTest {
+import static org.junit.Assert.*;
+
+public class PalindromeTest extends PalindromeCheck{
+
 
 
     @Test
     public void whenPalindromeIsCalling_ThenPalindromeIsReturned() {
-        String palindromeWord = PalindromeCheck.isPalindrome("121");
-        String palindromeSentenceTwoWords = PalindromeCheck.isPalindrome("top spot");
-        String palindromeSentenceManyWords = PalindromeCheck.isPalindrome("was it a cat i saw");
-        String palindromeSentenceCapitalLetters = PalindromeCheck.isPalindrome("NO LEMON NO MELON");
-        String palindromeNumbers = PalindromeCheck.isPalindrome("02 02 2020");
-        String palindromeNumbersWithMinus = PalindromeCheck.isPalindrome("-121");
+//        boolean palindromeWord = PalindromeCheck.isPalindrome("121");
+//        boolean palindromeSentenceTwoWords = PalindromeCheck.isPalindrome("top spot");
+//        boolean palindromeSentenceManyWords = PalindromeCheck.isPalindrome("Was it a cat i saw");
+//        boolean palindromeSentenceCapitalLetters = PalindromeCheck.isPalindrome("NO LEMON NO MELON");
+//        boolean palindromeNumbers = PalindromeCheck.isPalindrome("02 02 2020");
+//        boolean palindromeNumbersWithMinus = PalindromeCheck.isPalindrome("-121");
 
 
-        assertEquals("121", palindromeWord);
-        assertEquals("top spot", palindromeSentenceTwoWords);
-        assertEquals("was it a cat i saw", palindromeSentenceManyWords);
-        assertEquals("NO LEMON NO MELON", palindromeSentenceCapitalLetters);
-        assertEquals("02 02 2020", palindromeNumbers);
-        assertEquals("-121", palindromeNumbersWithMinus);
+
+        assertTrue(isPalindrome("121"));
+        assertFalse(isPalindrome("122"));
+        assertTrue(isPalindrome("02022020"));
+        assertFalse(isPalindrome("122"));
+        assertFalse(isPalindrome("122"));
+        assertFalse(isPalindrome("122"));
+//        assertEquals("121", palindromeWord);
+//        assertEquals("top spot", palindromeSentenceTwoWords);
+//        assertEquals("Was it a cat i saw", palindromeSentenceManyWords);
+//        assertEquals("NO LEMON NO MELON", palindromeSentenceCapitalLetters);
+//        assertEquals("02 02 2020", palindromeNumbers);
+//        assertEquals("-121", palindromeNumbersWithMinus);
+
+//        Boolean palindromeCheckNumber = new PalindromeCheck().check("225");
+//        assertTrue("225", true);
 
 
 
