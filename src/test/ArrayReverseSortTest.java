a3176db84ba6b7faa0ea7e2adb4acab64c72b60d
denis <anonim.ceo@gmail.com>
2020-03-21 19:45:10 +0200

    refactor

---
+++ src/test/ArrayReverseSortTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayReverseSortTest {
+
+    @Test
+    public void reverse() {
+
+    }
+
+    @Test
+    public void sort() {
+    }
+}
